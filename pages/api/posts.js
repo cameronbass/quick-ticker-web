418d463efad045509dde2bb7413f127f88befd3e
Cameron Bass <cam@advisoryalpha.com>
2022-09-30 09:18:37 -0400

    Reach out to posts

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -2,15 +2,21 @@ import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
   const client = await clientPromise;
-  const db = client.db("nextjs-mongodb-demo");
+  const db = client.db("quick_ticker_db");
+
   switch (req.method) {
     case "POST":
       let bodyObject = JSON.parse(req.body);
       let myPost = await db.collection("posts").insertOne(bodyObject);
+
       res.json(myPost.ops[0]);
       break;
     case "GET":
-      const allPosts = await db.collection("allPosts").find({}).toArray();
+      const allPosts = await db
+        .collection("posts")
+        .find({})
+        .toArray();
+
       res.json({ status: 200, data: allPosts });
       break;
   }
